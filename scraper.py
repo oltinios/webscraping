c344c99d3b57de8571656f325020c18a37227071
oltinios <80402091+oltinios@users.noreply.github.com>
2026-02-22 02:03:20 +0000

    Added parsing and a while loop to see how many products there are and how the syntax works

--- scraper.py
+++ scraper.py
@@ -5,4 +5,17 @@ url = "https://www.diy.com/painting-decorating/paint.cat?Location=Interior"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"}
 
 resp = httpx.get(url, headers=headers)
-print(resp)
+html = HTMLParser(resp.text)
+
+# print(html.css_first("title").text())
+
+products = html.css("div.mb-3xl ul li") # "mb-3xl" is the results for B&Q site
+print(products)
+i = 0
+j = 0
+
+while i < len(products):
+    j += 1
+    i += 1
+
+print(j)
