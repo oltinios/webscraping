6ba639ecc218e981d5d8810b71a1d1a2f5b93687
oltinios <80402091+oltinios@users.noreply.github.com>
2026-02-22 14:03:25 +0000

    Added functions to make code clearer

--- scraper.py
+++ scraper.py
@@ -1,37 +1,34 @@
 import httpx
 from selectolax.parser import HTMLParser
 
-url = "https://www.diy.com/painting-decorating/paint.cat?Location=Interior"
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"}
+def get_html():
+    url = "https://www.diy.com/painting-decorating/paint.cat?Location=Interior"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"}
 
-resp = httpx.get(url, headers=headers)
-html = HTMLParser(resp.text)
+    resp = httpx.get(url, headers=headers)
+    html = HTMLParser(resp.text)
+    return html
 
-# print(html.css_first("title").text())
+def parse_page(html):
+    products = html.css('li[class="max-md:border-b-size-sm max-md:border-color-default"]') # need to wrap it up because most parsers cant handle colons in the class names
 
-products = html.css('li[class="max-md:border-b-size-sm max-md:border-color-default"]') # need to wrap it up because most parsers cant handle colons in the class names
-i = 0
-j = 0
-
-while i < len(products):
-    j += 1
-    i += 1
+    for product in products:
+        item = {
+            "name": extract_text(product, ".mb-xs.font-bold"),
+            "price": extract_text(product, "span[data-testid=product-price]"),
+            "offers": extract_text(product, "span[data-testid=roundel]")
+        }
+        print(item)
 
 def extract_text(html, sel):
     try:
         return html.css_first(sel).text()
     except AttributeError:
         return None
-        
 
-for product in products:
-    item = {
-        "name": extract_text(product, ".mb-xs.font-bold"),
-        "price": extract_text(product, "span[data-testid=product-price]"),
-        "offers": extract_text(product, "span[data-testid=roundel]")
-    }
-    # print(product.css_first(".mb-xs.font-bold").text())
+def main():
+    html = get_html()
+    parse_page(html)
 
-    print(item)
-
-print(j, "Products")
+if __name__ == "__main__":
+    main()
